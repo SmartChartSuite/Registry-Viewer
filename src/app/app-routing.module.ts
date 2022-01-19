49db2602604fdd6b7ff86a886af82882276b8210
ptassev3 <ptassev3@gatech.edu>
2022-01-19 10:59:56 -0500

    Wired the navigation to the existing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SearchComponent} from "./components/search/search.component";
+import {AboutComponent} from "./components/about/about.component";
+import {LoginComponent} from "./components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  //We need to add Login guard to this section
+  {
+    path: '',
+    component: SearchComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  { // This path MUST ALWAYS be the last path!!!
+    // Do not add any paths below this point or they will not work and will be redirected to landing.
+    path: '**', redirectTo: ''
+  }
+  // End Routes Secured by LoginGuard
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,35 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 import { SearchComponent } from './components/search/search.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule, MatNavList} from "@angular/material/list";
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    SearchComponent
+    SearchComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    //Material design components import
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
