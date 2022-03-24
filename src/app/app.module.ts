47b97e55d7b9542b15ce90b3e6f52b70c665673d
Plamen Tassev <plamen@gatech.edu>
2022-03-24 11:07:04 -0400

    Added basic details dialog. Right now it is just a window with a sample text.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LoginComponent} from './components/login/login.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
-import { AboutComponent } from './components/about/about.component';
-import { CaseExplorerComponent } from './components/case-explorer/case-explorer.component';
+import {AboutComponent} from './components/about/about.component';
+import {CaseExplorerComponent} from './components/case-explorer/case-explorer.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatTableModule} from "@angular/material/table";
@@ -18,19 +18,19 @@ import {MatPaginatorModule} from "@angular/material/paginator";
 import {HttpClientModule} from "@angular/common/http";
 import {MatSortModule} from "@angular/material/sort";
 import {MatInputModule} from "@angular/material/input";
-import { RegistryViewerComponent } from './components/registry-viewer/registry-viewer.component';
+import {RegistryViewerComponent} from './components/registry-viewer/registry-viewer.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatListModule} from "@angular/material/list";
-import { TestComponent } from './components/test/test.component';
+import {TestComponent} from './components/test/test.component';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
-import { AnnotationDialogComponent } from './components/annotation-dialog/annotation-dialog.component';
+import {AnnotationDialogComponent} from './components/annotation-dialog/annotation-dialog.component';
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatSelectModule} from "@angular/material/select";
-import { DetailsDialogComponent } from './components/details-dialog/details-dialog.component';
+import {DetailsDialogComponent} from './components/details-dialog/details-dialog.component';
 
 @NgModule({
   declarations: [
@@ -71,4 +71,5 @@ import { DetailsDialogComponent } from './components/details-dialog/details-dial
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
