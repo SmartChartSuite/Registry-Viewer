61e2ae295edbbf5aa6523bc2f33ea704f6e02c04
Plamen Tassev <plamen@gatech.edu>
2022-04-30 16:10:15 -0400

    Not super happy about it, but the results are working.

--- src/app/components/registry-viewer/details-view/lab-results/lab-results.component.ts
+++ src/app/components/registry-viewer/details-view/lab-results/lab-results.component.ts
@@ -1,89 +1,94 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
+import {CaseRecordsService} from "../../../../service/case-records.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {ChronologicalCaseRecord} from "../../../../model/chronological.case.record";
+import {MatTableDataSource} from "@angular/material/table";
+import {SidenavService} from "../../../../service/sidenav.service";
 
 @Component({
   selector: 'app-lab-results',
   templateUrl: './lab-results.component.html',
   styleUrls: ['./lab-results.component.css', '../../registry-viewer.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class LabResultsComponent implements OnInit{
 
   @Input() matCardContentHeight: number;
 
-  constructor(private dialog: MatDialog) { }
+  panelOpenState = false;
 
-  ngOnInit(): void {
+  constructor(
+    private caseRecordsService: CaseRecordsService,
+    private sidenavService: SidenavService
+  ) { }
+
+  data: ChronologicalCaseRecord[];
+  columnsToDisplay = ['category'];
+  expandedRow: any | null;
+  dataSource: MatTableDataSource<ChronologicalCaseRecord>;
+  innerTableDisplayColumns = ['date', 'question'];
+
+  private extractCategories(data: any, key: string): string[]{
+    let result : string[] = [];
+    data.forEach((element: any)=> {
+      if(result.indexOf(element[key]) === -1){
+        result.push(element[key]);
+      }
+    });
+    return result;
   }
 
-  onEditAnnotation(value: any): void {
+
+  private groupByCategories(categories: string[], data: ChronologicalCaseRecord[]): any[] {
+    let groupedData = [];
+    categories.forEach(category => {
+      let obj : any = {};
+      obj.category = category;
+      obj.data = []
+      this.data.forEach(element => {
+        if(element.category === category){
+          obj.data.push(element);
+        }
+      });
+      obj.count = obj.data.length;
+      obj.expanded = true;
+      groupedData.push(obj);
+    })
+    return groupedData;
+  }
+
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource([]);
+
+    this.caseRecordsService.caseRecordChronologicalData$.subscribe({
+        next: value => {
+            this.data = value;
+            if (this.data?.length > 0) {
+              this.dataSource = new MatTableDataSource(this.data);
+              this.data = value?.filter(element => element.section === 'Lab Results');
+              this.data = this.data?.sort((a, b) => (a.date < b.date) ? 1 : -1);
+              const categories = this.extractCategories(this.data, 'category');
+              const categorised = this.groupByCategories(categories, this.data);
+              this.dataSource.data = categorised;
+            }
+        }
+      }
+    )
   }
 
   onViewDetails(value: any): void {
   }
 
-  labResultsFormatted = [
-    {
-      "group": "Syphilis",
-      "values" : [
-        {
-          "date": "2022-01-14T05:00:00.000Z",
-          "test": "Reagin Ab [Presence] in Serum by RPR",
-          "result": "Reactive"
-        },
-        {
-          "date": "2022-01-14T05:00:00.000Z",
-          "test": "Reagin Ab [Titer] in Serum by RPR",
-          "result": "1:4"
-        },
-        {
-          "date": "2021-07-10T04:00:00.000Z",
-          "test": "Reagin Ab [Titer] in Serum by RPR",
-          "result": "1:64"
-        },
-        {
-          "date": "2022-01-14T05:00:00.000Z",
-          "test": "Reagin Ab [Presence] in Serum by RPR",
-          "result": "Reactive"
-        },
-        {
-          "date": "2022-01-14T05:00:00.000Z",
-          "test": "Reagin Ab [Titer] in Serum by RPR",
-          "result": "1:4"
-        },
-        {
-          "date": "2021-07-10T04:00:00.000Z",
-          "test": "Reagin Ab [Titer] in Serum by RPR",
-          "result": "1:64"
-        },
-        {
-          "date": "2022-01-14T05:00:00.000Z",
-          "test": "Reagin Ab [Presence] in Serum by RPR",
-          "result": "Reactive"
-        },
-        {
-          "date": "2022-01-14T05:00:00.000Z",
-          "test": "Reagin Ab [Titer] in Serum by RPR",
-          "result": "1:4"
-        }
-      ],
-    },
-    {
-      "group": "HIV",
-      "values" : [
-        {
-          "date": "2022-01-14T05:00:00.000Z",
-          "test": "HIV 1+2 Ab [Presence] in Serum",
-          "result": "Negative"
-        },
-        {
-          "date": "2022-01-14T05:00:00.000Z",
-          "test": "HIV 1+2 Ab [Presence] in Serum",
-          "result": "Negative"
-        }
-      ],
-    },
 
-  ]
-
-
+  onRowClick(element: any) {
+    this.sidenavService.open();
+    this.sidenavService.setSidenavData(element);
+  }
 }
