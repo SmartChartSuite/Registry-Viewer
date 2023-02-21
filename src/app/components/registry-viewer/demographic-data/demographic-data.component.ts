aa689f08afe4f8f7b2c1eb0f25b1732a70f2a601
Plamen Tassev <plamen@gatech.edu>
2023-02-21 17:41:44 -0500

    Demo mode header implementation

--- src/app/components/registry-viewer/demographic-data/demographic-data.component.ts
+++ src/app/components/registry-viewer/demographic-data/demographic-data.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit } from '@angular/core';
 import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 import {CaseRecordsService} from "../../../service/case-records.service";
 import {ActivatedRoute} from "@angular/router";
+import {DemoModeService} from "../../../service/demo-mode.service";
 
 @Component({
   selector: 'app-demographic-data',
@@ -13,12 +14,13 @@ export class DemographicDataComponent implements OnInit {
   cols: number = 3;
   width: string = "8em";
   demographicsData: any;
-
+  isDemoModeActive = false;
 
   constructor(
     private route: ActivatedRoute,
     private responsive: BreakpointObserver,
-    private caseRecordService: CaseRecordsService
+    private caseRecordService: CaseRecordsService,
+    private demoModeService: DemoModeService
   ) { }
 
   ngOnInit(): void {
@@ -28,6 +30,10 @@ export class DemographicDataComponent implements OnInit {
       next: value => this.demographicsData = value?.data[0]
       }
     );
+
+    this.demoModeService.isDemoModeActive$.subscribe({
+      next: value => this.isDemoModeActive = value
+    })
   }
 
 }
