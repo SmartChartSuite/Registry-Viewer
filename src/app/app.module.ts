bbef6c788a3101524baf26aa1c0e58192bc6b477
ptassev3 <plamentassev@gmail.com>
2025-03-07 16:25:17 -0500

    updated a few packages for the multisort table to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { DetailsComponent } from './components/registry-viewer/side-panel/detail
 import { FlagComponent } from './components/registry-viewer/side-panel/flag/flag.component';
 import { AnnotationsComponent } from './components/registry-viewer/side-panel/annotations/annotations.component';
 import {DrawerService} from "./service/drawer.service";
-import {MatMultiSortModule} from "ngx-mat-multi-sort";
+import {MatMultiSort, MatMultiSortModule, MatMultiSortTableSettingsComponent} from "ngx-mat-multi-sort";
 import {AddRecordDialogComponent} from './components/registry-viewer/add-record-dialog/add-record-dialog.component';
 import {DatePipe, NgOptimizedImage} from "@angular/common";
 import { SectionComponent } from './components/registry-viewer/summary-view/section/section.component';
@@ -131,6 +131,8 @@ export const configFactory = (configService: ConfigService) => {
     MatRadioModule,
     MatTabGroup,
     MatTab,
+    MatMultiSortTableSettingsComponent,
+    MatMultiSort,
   ],
   providers: [
     ConfigService,
